6aab3f5875b1882693a1d4402a9e4b12271834d2
RenaudRoyBeaudoin <renaudroybeaudoin@gmail.com>
2023-05-10 14:44:05 -0400

    Timer et changement de scène fin

---
+++ Assets/Scripts/GestionnaireScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GestionnaireScene : MonoBehaviour
+{
+
+    private int _sceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void SceneSuivante(){
+        if(_sceneIndex < SceneManager.sceneCountInBuildSettings - 1){
+            SceneManager.LoadScene(_sceneIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Niv01/Timer.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private GestionnaireScene _gestionScenes;
+
+    [SerializeField] private TMP_Text _timeText;
+
+    [SerializeField] private float _tempsRestant;
+
+
+    //Vient chercher mon canvas FinDeJeu
+    [SerializeField] private GameObject _messageFin;
+
+    //Vient chercher mes autres canvas
+    [SerializeField] private GameObject[] _autresCanvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculTemps();
+        FinDeLaPartie();
+    }
+
+
+
+    private void CalculTemps(){
+        _tempsRestant -= Time.deltaTime;
+        AfficheTemps(_tempsRestant);
+    }
+
+    private void AfficheTemps(float temps){
+
+        temps +=1;
+
+        float minutes = Mathf.FloorToInt(temps/60);
+
+        float secondes = Mathf.FloorToInt(temps % 60);
+
+
+        _timeText.text = string.Format("{0:00}:{1:00}", minutes, secondes);
+    }
+
+
+
+    
+    private void FinDeLaPartie(){
+        if(_tempsRestant < 0){
+            //Active mon message de fin
+            _messageFin.SetActive(true);
+
+            //Désactive les autres éléments de mon UI
+            foreach(GameObject canvas in _autresCanvas){
+                canvas.SetActive(false);
+            }
+
+            //Passe à la scène suivante dans trois secondes
+            Invoke("SceneFin", 5);
+        }
+    }
+    private void SceneFin(){
+        _gestionScenes.SceneSuivante();
+    }
+}
